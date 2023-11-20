bae74e60304308e717ed6f18ea09317895d0f8e4
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-11-20 13:44:53 +0200

    s

---
+++ index.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt("How much moveis you watching", " ");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  moveis: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("One of the last movies I watched", "");
+const b = prompt("Please rate it", "");
+const c = prompt("One of the last movies I watched", "");
+const d = prompt("Please rate it", "");
+
+personalMovieDB.moveis[a] = b;
+personalMovieDB.moveis[c] = d;
+
+console.log(personalMovieDB);
